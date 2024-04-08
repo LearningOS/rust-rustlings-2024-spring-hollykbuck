2b869515d91995373cfa2f5291e6d33e744119bb
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2024-04-08 12:33:49 +0800

    Fix lifetime annotations, implement generics, and update traits

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -16,18 +16,41 @@
 //
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+pub trait ReportCard {
+    fn print(&self) -> String;
+}
 
-pub struct ReportCard {
+pub struct NumericReportCard {
     pub grade: f32,
     pub student_name: String,
     pub student_age: u8,
 }
 
-impl ReportCard {
-    pub fn print(&self) -> String {
+impl ReportCard for NumericReportCard {
+    fn print(&self) -> String {
         format!("{} ({}) - achieved a grade of {}",
             &self.student_name, &self.student_age, &self.grade)
+    }
+}
+
+pub struct AlphabeticReportCard {
+    pub grade: f32,
+    pub student_name: String,
+    pub student_age: u8,
+}
+
+impl ReportCard for AlphabeticReportCard {
+    fn print(&self) -> String {
+        let grade = match self.grade {
+            1.0..=1.9 => "F-",
+            2.0..=2.9 => "D-",
+            3.0..=3.9 => "C-",
+            4.0..=4.9 => "B-",
+            5.0..=5.5 => "A-",
+            _ => "A+",
+        };
+        format!("{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, grade)
     }
 }
 
@@ -37,7 +60,7 @@ mod tests {
 
     #[test]
     fn generate_numeric_report_card() {
-        let report_card = ReportCard {
+        let report_card = NumericReportCard {
             grade: 2.1,
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
@@ -51,8 +74,8 @@ mod tests {
     #[test]
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
-        let report_card = ReportCard {
-            grade: 2.1,
+        let report_card = AlphabeticReportCard {
+            grade: 6.0,
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
