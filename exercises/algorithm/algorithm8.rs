b0b520974652d8758d67f9536ba427c00d3ae055
hollykbuck <101749900+hollykbuck@users.noreply.github.com>
2024-04-09 14:12:32 +0800

    Refactor stack and queue implementations

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,6 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -54,7 +53,6 @@ impl<T> Default for Queue<T> {
 
 pub struct myStack<T>
 {
-	//TODO
 	q1:Queue<T>,
 	q2:Queue<T>
 }
@@ -67,15 +65,21 @@ impl<T> myStack<T> {
         }
     }
     pub fn push(&mut self, elem: T) {
-        //TODO
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
-        //TODO
-		Err("Stack is empty")
+        if self.is_empty() {
+            return Err("Stack is empty");
+        }
+        while self.q1.size() > 1 {
+            self.q2.enqueue(self.q1.dequeue().unwrap());
+        }
+        let result = self.q1.dequeue().unwrap();
+        std::mem::swap(&mut self.q1, &mut self.q2);
+        Ok(result)
     }
     pub fn is_empty(&self) -> bool {
-		//TODO
-        true
+        self.q1.is_empty() && self.q2.is_empty()
     }
 }
 
